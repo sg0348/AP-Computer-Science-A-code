cf82ad3feed1068908c7e3d2cb57dfedbafebed4
sg0348 <srishti.ganesh@gmail.com>
2026-01-02 13:24:36 -0700

    methods declared

--- src/main/java/org/ap/utils/Logger.java
+++ src/main/java/org/ap/utils/Logger.java
@@ -1,4 +1,15 @@
 package org.ap.utils;
-
 public class Logger {
+    public static void info()
+    {
+
+    }
+    public static void warn()
+    {
+
+    }
+    public static void error()
+    {
+        
+    }
 }
