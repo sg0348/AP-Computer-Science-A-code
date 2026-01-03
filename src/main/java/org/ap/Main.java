7b0595454286aa4579dc119659479c90895245c0
sg0348 <srishti.ganesh@gmail.com>
2026-01-02 22:27:31 -0700

    logger class implemented

--- src/main/java/org/ap/Main.java
+++ src/main/java/org/ap/Main.java
@@ -1,17 +1,17 @@
 package org.ap;
 
+import org.ap.utils.Logger;
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        Logger logger = new Logger();
+        logger.info("Parsing CSV file");
+        logger.warn("Bad corrupt file");
+        logger.error("Unable to parse file");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
     }
 }
--- src/main/java/org/ap/utils/Logger.java
+++ src/main/java/org/ap/utils/Logger.java
@@ -1,15 +1,35 @@
 package org.ap.utils;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 public class Logger {
-    public static void info()
+    public static String setTime()
     {
-
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        String timestamp = null;
+        try {
+            timestamp = now.format(formatter);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return timestamp;
     }
-    public static void warn()
+    public static void info(String message)
     {
-
+        System.out.print(setTime());
+        System.out.print("[INFO]");
+        System.out.println(message);
     }
-    public static void error()
+    public static void warn(String message)
     {
-        
+        System.out.print(setTime());
+        System.out.print("[WARN]");
+        System.out.println(message);
+    }
+    public static void error(String message)
+    {
+        System.out.print(setTime());
+        System.out.print("[ERROR]");
+        System.out.println(message);
     }
 }
