13e74d31a4e78de933a6ef0e3765a0ca7edd04d0
Srishti Ganesh <srishti.ganesh@gmail.com>
2026-01-12 13:31:41 -0700

    chore: fixing main function signature to be compatible with java 21

--- src/main/java/org/ap/utils/CsvReaderMain.java
+++ src/main/java/org/ap/utils/CsvReaderMain.java
@@ -2,9 +2,9 @@ package org.ap.utils;
 
 public class CsvReaderMain {
 
-    static void main () {
-        CSVReader csvReader = new CSVReader("/Users/srishtiganesh/test/grades.csv");
-        String[][] entries = csvReader.readAll();
-        System.out.println(entries);
-    }
+         public static void main(String[] args) {
+            CSVReader csvReader = new CSVReader("/Users/srishtiganesh/test/grades.csv");
+            String[][] entries = csvReader.readAll();
+            System.out.println(entries);
+        }
 }
--- src/main/java/org/ap/utils/LoggerMain.java
+++ src/main/java/org/ap/utils/LoggerMain.java
@@ -3,7 +3,7 @@ package org.ap.utils;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class LoggerMain {
-    static void main()
+    public static void main(String[] args)
     {
         Logger logger = new Logger("/Users/srishtiganesh/logs/out.log");
         logger.info("Parsing CSV file");
