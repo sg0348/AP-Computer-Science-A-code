7061e52e9e32c956e6b1620b6de10411125a4b7c
Srishti Ganesh <srishti.ganesh@gmail.com>
2026-01-19 13:55:00 -0700

    Implementing CSVRecord

---
+++ src/main/java/org/ap/utils/CSVRecord.java
@@ -0,0 +1,23 @@
+package org.ap.utils;
+
+public class CSVRecord
+{
+    private String[] headers;
+    private String[] fields;
+    public CSVRecord(String[] headers, String row)
+    {
+        this.headers=headers;
+        this.fields=row.split(",");
+    }
+
+    public String get(String headerName)
+    {
+        int i=0;
+        for(;i<headers.length;i++)
+        {
+            if (headers[i].equalsIgnoreCase(headerName))
+                return this.fields[i];
+        }
+        return "No such header exists";
+    }
+}
